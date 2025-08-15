e2d3e92afa371b9ccd79f34c355bc539baf462a0
Dario Didi <dididario.dd2@gmail.com>
2025-08-15 11:43:46 +0300

    feat: handle multiple file args

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,8 @@ use codecrafters_grep::match_by_char;
 fn main() {
     eprintln!("Logs from your program will appear here!");
 
-    let all_args = env::args();
+    let mut all_args = env::args();
+    let len_args = all_args.len();
     eprintln!("args:{:?}", all_args);
     if env::args().nth(1).unwrap() != "-E" {
         println!("Expected first argument to be '-E'");
@@ -19,10 +20,13 @@ fn main() {
     let mut res = false;
     let pattern = env::args().nth(2).unwrap();
 
-    if all_args.len() > 3 {
-        let mut loop_args = all_args.into_iter();
+    if len_args > 3 {
+        for _ in 0..3 {
+            let _ = all_args.next();
+        }
 
-        while let Some(input_file) = &loop_args.next() {
+        while let Some(input_file) = &all_args.next() {
+            eprintln!("checking file:{input_file}");
             let input_file = std::env::current_dir().unwrap().join(&input_file);
             if input_file.exists() {
                 let file = File::open(input_file).unwrap();
