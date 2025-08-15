d0c85513254670f98a107b0ff39bc1b167d98c6c
Dario Didi <dididario.dd2@gmail.com>
2025-08-15 11:27:37 +0300

    fix: return true if any line is found

--- src/main.rs
+++ src/main.rs
@@ -9,35 +9,39 @@ use codecrafters_grep::match_by_char;
 fn main() {
     eprintln!("Logs from your program will appear here!");
 
-    let mut all_args = env::args();
+    let all_args = env::args();
     eprintln!("args:{:?}", all_args);
     if env::args().nth(1).unwrap() != "-E" {
         println!("Expected first argument to be '-E'");
         process::exit(1);
     }
 
+    let mut res = false;
     let pattern = env::args().nth(2).unwrap();
 
-    let mut res = false;
-    if let Some(a) = all_args.nth(3) {
-        let input_file = std::env::current_dir().unwrap().join(&a);
-        if input_file.exists() {
-            let file = File::open(input_file).unwrap();
-            let reader = BufReader::new(file);
+    if all_args.len() > 3 {
+        let mut loop_args = all_args.into_iter();
 
-            reader.lines().for_each(|l| {
-                if let Ok(input_line) = l {
-                    //eprintln!("for line:{input_line}");
-                    res = match_by_char(&input_line, &pattern, false, &Vec::new()).0;
-                    if res {
-                        println!("{input_line}");
+        while let Some(input_file) = &loop_args.next() {
+            let input_file = std::env::current_dir().unwrap().join(&input_file);
+            if input_file.exists() {
+                let file = File::open(input_file).unwrap();
+                let reader = BufReader::new(file);
+
+                reader.lines().for_each(|l| {
+                    if let Ok(input_line) = l {
+                        //eprintln!("for line:{input_line}");
+                        let curr_res = match_by_char(&input_line, &pattern, false, &Vec::new()).0;
+                        if curr_res {
+                            println!("{input_line}");
+                        }
+                        res = res || curr_res;
                     }
-                }
-            });
+                });
+            }
         }
     } else {
         let mut input_line = String::new();
-
         io::stdin().read_line(&mut input_line).unwrap();
         res = match_by_char(&input_line, &pattern, false, &Vec::new()).0;
     }
